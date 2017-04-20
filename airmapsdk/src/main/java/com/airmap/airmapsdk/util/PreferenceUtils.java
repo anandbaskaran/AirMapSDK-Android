0ec38b84d09490e364e7ef427a87b81706c4873a
Vansh Gandhi <vansh@airmap.com>
2017-04-20 13:08:33 -0700

    Switch to modified copy of securepreferences, collapse exception catch

--- airmapsdk/src/main/java/com/airmap/airmapsdk/util/PreferenceUtils.java
+++ airmapsdk/src/main/java/com/airmap/airmapsdk/util/PreferenceUtils.java
@@ -2,22 +2,6 @@ package com.airmap.airmapsdk.util;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.util.Log;
-
-import java.io.IOException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.UnrecoverableEntryException;
-import java.security.cert.CertificateException;
-import java.util.Map;
-import java.util.Set;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.NoSuchPaddingException;
 
 import devliving.online.securedpreferencestore.RecoveryHandler;
 import devliving.online.securedpreferencestore.SecuredPreferenceStore;
@@ -33,10 +17,8 @@ public class PreferenceUtils {
     public static SharedPreferences getPreferences(Context context) throws SecuredPreferenceException {
         try {
             return SecuredPreferenceStore.getSharedInstance(context);
-        } catch (IOException | CertificateException | InvalidKeyException | NoSuchAlgorithmException | InvalidAlgorithmParameterException
-                | UnrecoverableEntryException | NoSuchProviderException | NoSuchPaddingException | KeyStoreException | RuntimeException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-
             throw new SecuredPreferenceException(e.getMessage());
         }
     }
